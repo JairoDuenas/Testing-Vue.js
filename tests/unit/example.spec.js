766ebca473d09126c5277717fbe10994d90b76de
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-18 09:38:08 -0500

    Jest

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,21 @@
 import { mount } from "@vue/test-utils";
 import App from "@/App.vue";
+import { huevo, espinaca } from "./jestTest";
 
 const wrapper = mount(App);
 const vm = wrapper.vm;
 console.log(vm.$data);
+
+describe("Se va a probar la calidad de los ingredientes", () => {
+  test("Calidad del huevo", () => {
+    expect(huevo().estado[0]).toBe("fresco");
+  });
+
+  test("Calidad de la espinaca", () => {
+    expect(espinaca()).toEqual({
+      estado: 1,
+
+      color: "verde",
+    });
+  });
+});
